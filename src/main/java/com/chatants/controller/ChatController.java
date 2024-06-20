cbbf9d139a88b55e4939e203a62cb1c091c34bce
samuel <mxw712@gmail.com>
2024-06-20 17:07:05 +0800

    feature: 实现了连续对话,但是对话开始的几次会漏字

--- src/main/java/com/chatants/controller/ChatController.java
+++ src/main/java/com/chatants/controller/ChatController.java
@@ -1,11 +1,19 @@
 package com.chatants.controller;
 
+import com.chatants.config.LocalCache;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description:
@@ -43,6 +51,52 @@ public class ChatController {
                 .user(message)
                 .stream()
                 .content();
+    }
+
+    /**
+     * 连续对话功能实现
+     */
+    @GetMapping(value = "/ai/stream/continue", produces = "text/event-stream;charset=UTF-8")
+    public Flux<String> continueChat(
+            @RequestParam(value = "userMessage", defaultValue = "给我讲个笑话") String userMessage,
+            @RequestParam(value = "chatId") String chatId
+    ) {
+        // 将chatId作为本轮对话的唯一标识,加上用户的输入,缓存本地,实现连续对话
+        // 先从本地缓存读取上一轮对话的chatId,如果有,则继续对话,如果没有,则重新开始对话
+        // 本地缓存可以使用redis,也可以使用内存缓存
+        if (LocalCache.CACHE.get(chatId) != null) {
+            // 从缓存中读取上一轮对话的chatId,然后继续对话
+            List<Message> messages = (List<Message>) LocalCache.CACHE.get(chatId);
+            messages.add(new UserMessage(userMessage));
+            StringBuilder assistMessageBuilder = new StringBuilder();
+            return chatClient.prompt(new Prompt(messages))
+                    .stream()
+                    .content()
+                    .doOnNext(assistMessageBuilder::append)
+                    .doFinally(
+                            signalType -> {
+                                messages.add(new AssistantMessage(assistMessageBuilder.toString()));
+                                System.out.println(assistMessageBuilder.toString());
+                                LocalCache.CACHE.put(chatId, messages);
+                            }
+                    );
+        } else {
+            // 本地为空,则发送新的对话请求,并缓存对话
+            List<Message> messages = new ArrayList<>();
+            messages.add(new UserMessage(userMessage));
+            StringBuilder assistMessageBuilder = new StringBuilder();
+
+            return chatClient.prompt(new Prompt(messages))
+                    .stream()
+                    .content()
+                    .doOnNext(assistMessageBuilder::append)
+                    .doFinally(signalType -> {
+                                messages.add(new AssistantMessage(assistMessageBuilder.toString()));
+                                System.out.println(assistMessageBuilder.toString());
+                                LocalCache.CACHE.put(chatId, messages);
+                            }
+                    );
+        }
     }
 
 }
