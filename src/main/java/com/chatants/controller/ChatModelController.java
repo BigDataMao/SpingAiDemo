1fbaca505186c7570e8527c121408eb2fd152afc
毛新伟 <mxw712@gmail.com>
2024-06-21 23:16:22 +0800

    feature: 实现了连续对话,但不优雅

--- src/main/java/com/chatants/controller/ChatModelController.java
+++ src/main/java/com/chatants/controller/ChatModelController.java
@@ -1,5 +1,8 @@
 package com.chatants.controller;
 
+import com.chatants.config.LocalCache;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -26,6 +31,10 @@ public class ChatModelController {
         this.openAiChatModel = openAiChatModel;
     }
 
+    /**
+     * @description 测试生成聊天
+     * @param message  用户输入的消息
+     */
     @GetMapping("/ai/generate")
     public Map<String,String> generate(
             @RequestParam(value = "message", defaultValue = "Tell me a joke") String message
@@ -33,6 +42,10 @@ public class ChatModelController {
         return Map.of("generation", openAiChatModel.call(message));
     }
 
+    /**
+     * @description 流式返回聊天,并能后端留存数据
+     * @param message  用户输入的消息
+     */
     @GetMapping("/ai/generateStream")
     public Flux<ChatResponse> generateStream(
             @RequestParam(value = "message", defaultValue = "用一个四字成语夸我") String message
@@ -42,5 +55,44 @@ public class ChatModelController {
         return openAiChatModel.stream(prompt)
                 .doOnNext(chatResponse -> assistantMessage.append(chatResponse.getResult().getOutput().getContent()))
                 .doOnComplete(() -> System.out.println("assistantMessage = " + assistantMessage));
+    }
+
+    /**
+     * @description 流式返回聊天,并能后端留存数据
+     * @param message  用户输入的消息
+     * @param sessionId  会话ID
+     */
+    @GetMapping("/ai/generateStreamWithSessionId")
+    public Flux<ChatResponse> generateStreamWithSessionId(
+            @RequestParam(value = "message", defaultValue = "用一个四字成语夸我") String message,
+            @RequestParam(value = "sessionId", defaultValue = "1") String sessionId
+    ) {
+        List<Message> messages = (List<Message>)LocalCache.CACHE.get(sessionId);
+        if (messages == null) {
+            messages = List.of(new UserMessage(message));
+        }else {
+            messages.add(new UserMessage(message));
+        }
+        System.out.printf("messages = %s%n", messages);
+
+        return getAnswer(sessionId, messages);
+    }
+
+
+    public Flux<ChatResponse> getAnswer(
+            String sessionId,
+            List<Message> incomingMessages
+    ){
+        List<Message> messages = new ArrayList<>(incomingMessages);
+        StringBuilder answer = new StringBuilder();
+        Prompt prompt = new Prompt(messages);
+        return openAiChatModel.stream(prompt)
+                .doOnNext(chatResponse -> answer.append(chatResponse.getResult().getOutput().getContent()))
+                .doOnComplete(() -> {
+                    System.out.printf("answer = %s%n", answer);
+                    messages.add(new AssistantMessage(answer.toString()));
+                    LocalCache.CACHE.put(sessionId, messages);
+                });
+
     }
 }
