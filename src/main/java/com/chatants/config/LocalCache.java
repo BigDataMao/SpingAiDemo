9abb74ecce985f021e875409eed43c7033748706
samuel <mxw712@gmail.com>
2024-06-22 17:36:18 +0800

    fix: 修正LocalCache读取后强转问题

--- src/main/java/com/chatants/config/LocalCache.java
+++ src/main/java/com/chatants/config/LocalCache.java
@@ -3,11 +3,15 @@ package com.chatants.config;
 import cn.hutool.cache.CacheUtil;
 import cn.hutool.cache.impl.TimedCache;
 import cn.hutool.core.date.DateUnit;
+import org.springframework.ai.chat.messages.Message;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * @Description:
- * @Author: Simon Mau
- * @Date: 2024/2/20 14:26
+ * @description 本地缓存
+ * @author Semuel Mau
+ * @date 2024/2/20 14:26
  */
 
 public class LocalCache {
@@ -23,6 +27,22 @@ public class LocalCache {
      * 缓存对象
      */
     public static final TimedCache<String, Object> CACHE = CacheUtil.newTimedCache(TIMEOUT);
+
+    /**
+     * 从缓存中获取消息列表
+     *
+     * @param key 缓存key
+     * @return 消息列表
+     */
+    @SuppressWarnings("unchecked")
+    public static List<Message> getMessageListFromCache(String key) {
+        Object cachedValue = CACHE.get(key);
+        if (cachedValue instanceof List) {
+            return (List<Message>) cachedValue;
+        } else {
+            return new ArrayList<>();
+        }
+    }
 
     static {
         //启动定时任务
